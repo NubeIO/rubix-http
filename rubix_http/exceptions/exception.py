1e24086c92c47bd2e586f24164ed166c0bc96406
RaiBnod <binodrayee@gmail.com>
2021-05-19 09:30:24 +0545

    Add exceptions

--- rubix_http/exceptions/exception.py
+++ rubix_http/exceptions/exception.py
@@ -1,13 +1,38 @@
-class NotFoundException(BaseException):
-    def __init__(self, *args):
-        super(NotFoundException, self).__init__(*args)
-
-
 class BadDataException(BaseException):
     def __init__(self, *args):
         super(BadDataException, self).__init__(*args)
 
 
+class UnauthorizedException(BaseException):
+    def __init__(self, *args):
+        super(UnauthorizedException, self).__init__(*args)
+
+
+class ForbiddenException(BaseException):
+    def __init__(self, *args):
+        super(ForbiddenException, self).__init__(*args)
+
+
+class NotFoundException(BaseException):
+    def __init__(self, *args):
+        super(NotFoundException, self).__init__(*args)
+
+
 class PreConditionException(BaseException):
     def __init__(self, *args):
         super(PreConditionException, self).__init__(*args)
+
+
+class InternalServerErrorException(BaseException):
+    def __init__(self, *args):
+        super(InternalServerErrorException, self).__init__(*args)
+
+
+class NotImplementedException(BaseException):
+    def __init__(self, *args):
+        super(NotImplementedException, self).__init__(*args)
+
+
+class GatewayTimeoutException(BaseException):
+    def __init__(self, *args):
+        super(GatewayTimeoutException, self).__init__(*args)
--- rubix_http/resource.py
+++ rubix_http/resource.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, abort
 from sqlalchemy.exc import IntegrityError, OperationalError
+from werkzeug.exceptions import BadRequest
 
-from rubix_http.exceptions.exception import NotFoundException, BadDataException, PreConditionException
+from rubix_http.exceptions.exception import *
 
 
 class RubixResource(Resource):
@@ -10,11 +11,21 @@ class RubixResource(Resource):
             return super().dispatch_request(*args, **kwargs)
         except (IntegrityError, OperationalError) as e:
             abort(400, message=str(e.orig))
-        except (ValueError, BadDataException) as e:
+        except (ValueError, BadRequest, BadDataException) as e:
             abort(400, message=str(e))
+        except UnauthorizedException as e:
+            abort(401, message=str(e))
+        except ForbiddenException as e:
+            abort(403, message=str(e))
         except NotFoundException as e:
             abort(404, message=str(e))
         except PreConditionException as e:
             abort(428, message=str(e))
+        except InternalServerErrorException as e:
+            abort(500, message=str(e))
+        except NotImplementedException as e:
+            abort(501, message=str(e))
+        except GatewayTimeoutException as e:
+            abort(503, message=str(e))
         except Exception as e:
             abort(500, message=str(e))
